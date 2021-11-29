c6eb59a9e61efe2891b3cd681474bb0c317dad06
gierot <gierot@hotmail.com>
2021-11-29 19:49:00 -0300

    tericero commit

--- projetoC#/projetodg/Program.cs
+++ projetoC#/projetodg/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
-
-namespace projetodg
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace flj
 {
+    
+    
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            console.WriteLine("mengo");
+           
         }
     }
 }
