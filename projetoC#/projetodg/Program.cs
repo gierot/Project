46368129e65ccf7a4311f165567e81d73b847cdb
gierot <gierot@hotmail.com>
2021-12-18 19:42:16 -0300

    quinto commt

--- projetoC#/projetodg/Program.cs
+++ projetoC#/projetodg/Program.cs
@@ -11,53 +11,56 @@ namespace aula01
         {
             valor = 500;
         }
-        public void Pass()
+        public void Pass(int quantidadepass)
         {
-        Inicio:
-            Console.Clear();
-            Console.WriteLine("Por favor, digite quantas passagens irá comprar");
-            int quantidadepass = int.Parse(Console.ReadLine());
             int valorTotal = quantidadepass * valor;
             obj.quantidade--;
-            Console.WriteLine("deseja comprar outra passagem?\n s/n");
-            string resposta = Console.ReadLine();
-            if (resposta == "s" || resposta == "S")
-            {
-                goto Inicio;
-
-            }
+            Console.WriteLine("o valor total das passagens é de {0}", valorTotal);
         }
 
 
     }
     class Destino
     {
-        protected string[] lugar = new string[12];
+        public string[] lugar { get; set; } = new string[12];
+        public int escolha;
         public Destino()
         {
             lugar[0] = "Espirito santo"; lugar[1] = "São paulo"; lugar[2] = "Amazonas";
             lugar[3] = "Recife"; lugar[4] = "Rio de Janeiro"; lugar[5] = "Bahia";
-            lugar[6] = "Florianópolis"; lugar[7] = "Salvador"; lugar[8] = "Minas Gerais";
-            lugar[9] = "Santa catarina"; lugar[10] = "Porto Alegre"; lugar[11] = "Coritiba";
-
+            lugar[6] = "Florianópolis"; lugar[7] = "Rio grande do sul"; lugar[8] = "Minas Gerais";
+            lugar[9] = "Santa catarina"; lugar[10] = "Parana"; lugar[11] = "Acre";
         }
-        public string lugares
+        public int valor
         {
             get
             {
-                return lugar[12];
+                return escolha;
             }
             set
             {
-                lugar[12] = value;
+                escolha = value;
             }
         }
+        public string[] info()
+        {
+            int i = 0;
+            for ( i = 0; i < lugar.Length; i++)
+            {
+                Console.WriteLine(lugar[i]);
+            }        
+            Console.WriteLine("Esolha um dos destinos para a sua viagem");
+            escolha = int.Parse(Console.ReadLine());
+            Console.WriteLine(lugar[escolha - 1]);
+            
+            return lugar;
+        }
+        
     }
     class Onibus
     {
-
         protected int quant_passageiros_total;
-        protected float valor_passagem;
+        private int[,] assentos{ get; set;} = new int[2,5];
 
         public Onibus()
         {
@@ -74,14 +77,45 @@ namespace aula01
                 quant_passageiros_total = value;
             }
         }
+        
+    }
+    class Distancia
+    {
+        Destino obj = new Destino();
+        public string central = "Distrito federal";
+        public float[] distance { get; set; } = new float[12];
 
-
+        public Distancia()
+        {
+            distance[0] = 1441.6f; distance[1] = 1050.6f; distance[2] = 2262f; distance[3] = 2346.7f;
+            distance[4] = 1452.4f; distance[5] = 995f; distance[6] = 1723.9f; distance[7] = 1915.4f;
+            distance[8] = 642f; distance[9] = 1656.9f; distance[10] = 1423f; distance[11] = 2.441f;
+        }
+        public float[] relatorio()
+        {
+            int i = 0;
+            for (i = 0; i < 1; i++)
+            {
+                Console.WriteLine(obj.lugar[obj.escolha]);
+            }
+            //Console.WriteLine("A distancia é de {0}", distance[obj.escolha]);
+            return distance;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Destino tst = new Destino();
+            Distancia dst = new Distancia();
+            tst.info();
+            for (int i = 0; i < 1; i++)
+            {
+                
+            }
+            dst.relatorio();
+
         }
+        
     }
 }
